af88278553b9759fac7bec416dbb430e42f63cc4
stigsanek <stigsanek@yandex.ru>
2020-07-16 01:24:02 +0400

    Добавляет начальные данные для БД

--- migrations/Version20200715204922.php
+++ migrations/Version20200715204922.php
@@ -22,6 +22,8 @@ final class Version20200715204922 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, title LONGTEXT NOT NULL, deadline DATE NOT NULL, user_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('INSERT INTO task (title, deadline, user_id) VALUES ("Create REST API", "2020-07-20", 1), ("Drink coffee", "2020-07-21", 1), ("Buy bread", "2020-07-21", 2), ("Buy coffee", "2020-07-22", 3)');
+        $this->addSql('INSERT INTO user (first_name, last_name) VALUES ("Chris", "Evans"), ("Ben", "Parson"), ("Martin", "Flatcher")');
     }
 
     public function down(Schema $schema) : void
