03cffb61e07a80723b3f54dfc945a77dcb62be55
stigsanek <stigsanek@yandex.ru>
2020-07-16 01:49:14 +0400

    Добавляет контроллер задач. Реализует методы получения списка задач и одной задачи.

---
+++ src/Controller/TasksController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\Annotations as Route;
+use App\Entity\Task;
+
+class TasksController extends AbstractController
+{
+    /**
+     * @Route\Get("/tasks", name="get_all_tasks")
+     *
+     * Метод получения списка задач
+     */
+    public function getAllTasks()
+    {
+        $tasks = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findAll();
+
+        $data = [];
+        foreach ($tasks as $item) {
+            $data[] = [
+                'id' => $item->getId(),
+                'title' => $item->getTitle(),
+                'deadline' => $item->getDeadline(),
+                'user_id' => $item->getUserId()
+            ];
+        }
+
+        return $this->json([
+            'tasks' => $data
+        ]);
+    }
+
+    /**
+     * @Route\Get("/tasks/{id}", name="get_item_tasks")
+     *
+     * Метод получения задачи по id
+     */
+    public function getItemTasks($id)
+    {
+        $task = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->find($id);
+
+        if (!$task) {
+            return $this->json([
+                'message' => 'No task found for id ' . $id
+            ]);
+        }
+
+        return $this->json([
+            'id' => $task->getId(),
+            'title' => $task->getTitle(),
+            'deadline' => $task->getDeadline(),
+            'user_id' => $task->getUserId()
+        ]);
+    }
+}
