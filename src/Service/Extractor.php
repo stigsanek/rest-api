8c17da990d2cc0b4a626be7d946ebf43e7b4c32d
stigsanek <stigsanek@yandex.ru>
2020-07-16 13:24:12 +0400

    Реализует сервисный класс получения данных из запросов

---
+++ src/Service/Extractor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+class Extractor
+{
+    /**
+     * Статичный метод получения данных для всех видов запросов
+     * GET, POST, PUT, DELETE...
+     */
+    public static function extractData($method)
+    {
+        if ($method === 'GET') {
+            return $_GET;
+        }
+
+        if ($method === 'POST') {
+            return $_POST;
+        }
+
+        $data = [];
+        $exploded = explode('&', file_get_contents('php://input'));
+
+        foreach ($exploded as $pair) {
+            $item = explode('=', $pair);
+            if (count($item) == 2) {
+                $data[urldecode($item[0])] = urldecode($item[1]);
+            }
+        }
+
+        return $data;
+    }
+}
